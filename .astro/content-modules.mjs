a4c9c4458138d7105a0f9e243b09c3b3ade96fa3
Philipp Storl <phlp85@googlemail.com>
2026-06-17 20:23:07 +0200

    Clean repository hygiene: remove tracked node_modules and .astro; enforce forbidden tracked files in CI

--- .astro/content-modules.mjs
+++
@@ -1,7 +0,0 @@
-
-export default new Map([
-["src/content/work/brand-evolution.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Fbrand-evolution.mdx&astroContentModuleFlag=true")],
-["src/content/work/leadership-operating-model.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Fleadership-operating-model.mdx&astroContentModuleFlag=true")],
-["src/content/work/storyblok-migration.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Fstoryblok-migration.mdx&astroContentModuleFlag=true")],
-["src/content/work/voices-conference-website.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Fvoices-conference-website.mdx&astroContentModuleFlag=true")]]);
-		
